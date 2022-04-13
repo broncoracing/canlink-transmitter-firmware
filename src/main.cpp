6a89f3aa3b9960dcab8525be8d143f023a5d148c
Jack LeFevre <jackflef@gmail.com>
2022-04-13 17:48:43 -0400

    Added receive functionality

--- src/main.cpp
+++ src/main.cpp
@@ -16,10 +16,11 @@ extern DigitalOut led;
 
 // Prototypes
 void initCanMap();
+
 void updateCanMap(CANMessage *msg);
 
 // Main data store
-map<uint32_t, CANFrame> canData;
+map <uint32_t, CANFrame> canData;
 
 // Timer for spacing out rf transmissions
 Timer timer;
@@ -32,74 +33,153 @@ CANMessage inmsg;
 uint8_t packetBuf[sizeof(CANFrame) *
                   (sizeof(trackedCanIds) / sizeof(trackedCanIds[0]))];
 
+// Outgoing can messages go here
+CANMessage outmsg;
+
+char xbeeBuf = 0;       // UART bytes go here
+Queue<char, 512> xbeeQueue;
+
+// holds trial CAN frame before it is converted to an Mbed frame
+uint8_t frameBuf[sizeof(CANFrame)] = {0};
+
+
+// Prototypes
+void xbeeISR();
+
+void buildFrame(CANFrame *tempFrame);
+
+
 int main(void) {
-  initBoard();
-  initCanMap();
+    initBoard();
+    initCanMap();
 
-  timer.reset();
-  timer.start();
+    timer.reset();
+    timer.start();
 
-  while (1) {
-    // if a message is available, log it.
-    if (can.read(inmsg)) {
-      updateCanMap(&inmsg);
+
+    xbee.attach(xbeeISR, SerialBase::RxIrq); // dynamically set receive interrupt for XBEE transceiver
+
+
+    while (1) {
+        // TRANSMIT FUNCTIONALITY
+        // if a message is available, log it.
+        if (can.read(inmsg)) {
+            updateCanMap(&inmsg);
+        }
+
+        // time to transmit data
+        if (timer.read_ms() >= TRANSMIT_INTERVAL_MS) {
+            timer.reset();
+
+            // light LED while transmitting a packet
+            led.write(1);
+
+            // iterate through entire map
+            uint8_t i = 0;
+            for (const auto &[key, value]: canData) {
+                // copy each CANFrame struct to the output buffer
+                memcpy(packetBuf + i, &value, sizeof(value));
+                // offset output buffer destination by the size of the frame we just
+                // added
+                i += sizeof(CANFrame);
+            }
+            xbee.write(packetBuf, sizeof(packetBuf));
+
+            led.write(0);
+        }
+
+        // RECEIVE FUNCTIONALITY
+        if (xbeeQueue.full())                                               // if we fall this far behind there is an issue
+        {
+            char *dummy_out;
+            while(!xbeeQueue.empty()) {
+                xbeeQueue.try_get(&dummy_out);
+            }                                                 // wipe the buffer and attempt to recover
+        }
+
+        while (!xbeeQueue.empty())                       // make sure we have enough data to build a packet
+        {
+            CANFrame tempFrame = {0};                                       // empty trial frame
+
+            for (int i = 0; i < sizeof(CANFrame) - 1; i++)
+            {
+                frameBuf[i] = frameBuf[i + 1];
+            }
+
+            char *last; // new last byte for frame buf
+            if(xbeeQueue.try_get(&last)) {                                     // pull the latest value off the queue
+                frameBuf[sizeof(frameBuf) - 1] = *last;
+                memcpy(&tempFrame, frameBuf, sizeof(CANFrame));                 // build trial frame
+                buildFrame(&tempFrame);                                         // build output frame if the checksum is valid
+            } else {
+                break;
+            }
+
+        }
     }
-
-    // time to transmit data
-    if (timer.read_ms() >= TRANSMIT_INTERVAL_MS) {
-      timer.reset();
-
-      // light LED while transmitting a packet
-      led.write(1);
-
-      // iterate through entire map
-      uint8_t i = 0;
-      for (const auto &[key, value] : canData) {
-        // copy each CANFrame struct to the output buffer
-        memcpy(packetBuf + i, &value, sizeof(value));
-        // offset output buffer destination by the size of the frame we just
-        // added
-        i += sizeof(CANFrame);
-      }
-      xbee.write(packetBuf, sizeof(packetBuf));
-
-      led.write(0);
-    }
-  }
 }
 
 // Initialize with the values that we care about
 void initCanMap() {
-  // create a valid dummy frame (this way RX LED lights up even when car is off)
-  CANFrame dummyFrame = {0};
-  dummyFrame.syncWord = 0xA55A;
-  fillChecksum(&dummyFrame);
+    // create a valid dummy frame (this way RX LED lights up even when car is off)
+    CANFrame dummyFrame = {0};
+    dummyFrame.syncWord = 0xA55A;
+    fillChecksum(&dummyFrame);
 
-  // make volatile for debugger viewing
-  volatile int numElements = sizeof(trackedCanIds) / sizeof(trackedCanIds[0]);
-  for (int i = 0; i < numElements; i++) {
-    canData[trackedCanIds[i]] = dummyFrame;
-  }
+    // make volatile for debugger viewing
+    volatile int numElements = sizeof(trackedCanIds) / sizeof(trackedCanIds[0]);
+    for (int i = 0; i < numElements; i++) {
+        canData[trackedCanIds[i]] = dummyFrame;
+    }
 }
 
 // Takes an MBed style CAN message and updates the map if we care about the ID
 void updateCanMap(CANMessage *msg) {
-  CANFrame tempFrame = {0};
+    CANFrame tempFrame = {0};
 
-  // Don't add any new values that weren't in the map when we initialized it
-  if (canData.find(uint32_t(msg->id)) != canData.end()) {
-    // Copy data to serializable frame format
-    if (msg->format == CANExtended) {
-      tempFrame.extended = true;
-    } else {
-      tempFrame.extended = false;
+    // Don't add any new values that weren't in the map when we initialized it
+    if (canData.find(uint32_t(msg->id)) != canData.end()) {
+        // Copy data to serializable frame format
+        if (msg->format == CANExtended) {
+            tempFrame.extended = true;
+        } else {
+            tempFrame.extended = false;
+        }
+
+        tempFrame.id = msg->id;
+        tempFrame.dlc = msg->len;
+        memcpy(tempFrame.data, msg->data, msg->len);
+
+        fillChecksum(&tempFrame);
+        canData[tempFrame.id] = tempFrame;
     }
+}
 
-    tempFrame.id = msg->id;
-    tempFrame.dlc = msg->len;
-    memcpy(tempFrame.data, msg->data, msg->len);
 
-    fillChecksum(&tempFrame);
-    canData[tempFrame.id] = tempFrame;
-  }
+void xbeeISR()
+{
+    xbee.read((void*)&xbeeBuf, sizeof(xbeeBuf));
+    xbeeQueue.try_put(&xbeeBuf);
+}
+
+
+void buildFrame(CANFrame *tempFrame)
+{
+    if (tempFrame->syncWord == 0xA55A && validateChecksum(tempFrame))       // got a valid frame
+    {
+        outmsg.id = tempFrame->id;                                          // build an Mbed CAN frame and send it
+        outmsg.len = tempFrame->dlc;
+        memcpy(outmsg.data, tempFrame->data, tempFrame->dlc);
+
+        if (tempFrame->extended)
+        {
+            outmsg.format = CANExtended;
+        }
+        else
+        {
+            outmsg.format = CANStandard;
+        }
+
+        can.write(outmsg);
+    }
 }
